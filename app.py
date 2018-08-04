d2a41480ec57e259a37cf899517f588300f30a2c
Chris Rupley <crupley@salesforce.com>
2018-08-04 08:35:32 -0700

    add live reload for development

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import dash
+from dash.dependencies import Event, Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+# Edit this object!
+layout = html.Div([
+    html.H1('Hello Dash!'),
+    html.H3('Dash with live-reload'),
+
 
     html.Div(children='''
         Dash: A web application framework for Python.
@@ -25,6 +29,22 @@ app.layout = html.Div(children=[
         }
     )
 ])
+
+# does not refresh
+app.layout = html.Div(children=[
+    dcc.Interval(id='refresh', interval=1000),
+    html.Div(id='content', className="container")
+])
+
+# Update the `content` div with the `layout` object.
+# When you save this file, `debug=True` will re-run
+# this script, serving the new layout
+# https://community.plot.ly/t/is-there-an-interactive-development-environment-for-dash/7151/2
+@app.callback(
+    Output('content', 'children'),
+    events=[Event('refresh', 'interval')])
+def display_layout():
+    return layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
